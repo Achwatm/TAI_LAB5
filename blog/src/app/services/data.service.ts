d559da2d6589b72a4a054f6693bc4a91120380e0
Velkan <achwatm@gmail.com>
2019-04-26 10:23:34 +0200

    select

--- blog/src/app/services/data.service.ts
+++ blog/src/app/services/data.service.ts
@@ -15,5 +15,8 @@ getAll() {
   getById(id) {
     return this.http.get(this.url + '/api/posts/' + id);
   }
+  getByText(data){
+      return this.http.post(this.url + '/api/posts/',data);
+  }
 
 }
